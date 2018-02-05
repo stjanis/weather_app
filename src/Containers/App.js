2cf1f976a89416cfa39247376bfaa5df2fabfc18
Janis Stipnieks <j.stipnieks@gmail.com>
2018-02-05 22:56:43 +0100

    master: connect openweathermap API

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import '../Scss/main.css';
 
+const API_KEY = 'f4db44a7c0f5d0532e5ea1c3505317e0';
+
 class App extends Component {
+
+  componentDidMount() {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=Copenhagen&APPID=${API_KEY}`)
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
   render() {
     return (
       <div className="App">
